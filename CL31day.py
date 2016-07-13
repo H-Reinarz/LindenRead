447031bd3298ff8227efe087b0240febf48d2ee2
Henning <Henning-R@hotmail.de>
2016-07-13 23:54:09 +0200

    Initial commit!

---
+++ CL31day.py
@@ -0,0 +1,84 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Henning
+#
+# Created:     13.07.2016
+# Copyright:   (c) Henning 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+from collections import OrderedDict
+
+
+class CL31day:
+    """Class representing a whole day of records from the instrument.
+    The class stores the raw data and contains methods for calculating stats."""
+
+    def __init__(self, filename, raw_data):
+        self.filename = filename
+
+
+        self.records = OrderedDict()
+
+        for t in range (0,len(data)): #Run until last row of file (EOF)
+        #for t in range (0,300): #Run until last row of file (EOF)
+            entries = ["NA","NA","NA","NA","NA","NA"]
+            if data[t].find('Beginn:') != -1: # IF "Beginn: is found within the entry
+                #Date and Time
+                data[t]=data[t].strip("Beginn: ") #remove Beginn frm string
+                data[t]=data[t].strip("\n") #remove New-Line character
+                date_time= data[t] #Define remaining data as Date
+                #print date_time
+                date = date_time[0:10]
+                time = date_time[12:20]
+                #Save to List
+                entries[0] = date
+                entries[1] = time
+
+
+
+                #Measurements
+                status=data[t+2]    #Take next entry as
+                #print status
+                count=data[t+2][0]
+                if(count=="0"): #Zero means no Cloudbase detected
+                       entries[2] = "CLEAR"
+
+                if(count=="1"): #one cloudbase
+                    cloudbase=data[t+2][4:8]
+                    #print cloudbase
+                    entries[3] = cloudbase
+
+                if(count=="2"): #two cloudbases
+                    cloudbase=data[t+2][4:8]
+                    #print cloudbase
+                    entries[3] = cloudbase
+
+                    cloudbase_t=data[t+2][10:14]
+                    #print cloudbase_t
+                    entries[4] = cloudbase_t
+
+                if(count=="3"): # three cloudbases
+                    cloudbase=data[t+2][4:8]
+                    #print cloudbase
+                    entries[3] = cloudbase
+                    cloudbase_t=data[t+2][10:14]
+                    #print cloudbase_t
+                    entries[4] = cloudbase_t
+                    cloudbase_g=data[t+2][16:20]
+                    #print cloudbase_g
+                    entries[5] = cloudbase_g
+
+                if(count=="4"): # 4 in Fog (No cloudbase will be measured.... vertical visibility instead), Full obscuration
+                   entries[2] = "FOG"
+                   #print data[t+2]
+                if(count=="5"): #5 in Fog (No cloudbase will be measured.... vertical visibility instead), Partly opaque
+                    entries[2] = "FOG"
+                    #print data[t+2]
+
+
+            #Create CSV-Line
+            sep = ","
+            csv_line = sep.join(entries)
