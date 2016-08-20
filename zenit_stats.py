9479d5b729085afce36cb41ac733bc29daefafff
Henning <Henning-R@hotmail.de>
2016-08-20 10:16:28 +0200

    First running version!

--- zenit_stats.py
+++ zenit_stats.py
@@ -34,7 +34,7 @@ cl31_dir = "w:/Bendix/CL31/TEXT"
 stats_file = "d:\\Studium_EnvGEo\\Zweites_Semester\\Bendix\\Dev\\CL31_stats_zenit_60.csv"
 
 with open(stats_file, "w") as s:
-    s.write(CL31day.write_stats_header())
+    s.write(CL31day.write_stats_header() + ",ZENIT")
     s.write("\n")
 
     skipped = []
@@ -59,14 +59,16 @@ with open(stats_file, "w") as s:
                 Klasse = CL31day(filename, raw_data)
 
                 #Set window for analyzing
-                zenit = ts_decode(ZT[file[-12:-4]])
-                start = zenit - round((time_window*60)/2)
-                end = zenit + round((time_window*60)/2)
+                zenit = ts_decode(ZT[file[9:17]])
+                start = ts_encode(zenit - round((time_window*60)/2))
+                end = ts_encode(zenit + round((time_window*60)/2))
 
                 #Analyze
-                print(file[-12:-4], ZT[file[-12:-4]], zenit, start, end, round((time_window*60)/2))
+##                print(file[-12:-4], ZT[file[-12:-4]], zenit, start, end, round((time_window*60)/2))
                 Klasse.compute_stats(start, end)
 
+
+
                 #Write
-                s.write(Klasse.write_stat_string())
+                s.write(Klasse.write_stat_string() + ","+":".join(ZT[file[9:17]]))
                 s.write("\n")
