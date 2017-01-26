1b10ae5f8b0d2676e5a4eda7221a8d7c15e5b439
Guentheg <Guentheg@students.uni-marburg.de>
2017-01-26 12:02:44 +0100

    Clean up iqbal script wip

--- campbell/iqbal.py
+++ campbell/iqbal.py
@@ -2,32 +2,42 @@
 import calendar
 import subprocess
 
-
-file = r"H:/Informatik/PythonApplications/iqbal/iqbal/initial.dat"
+#### VARIABLES ####
+# Inital File from iqbal
+initalFile = r"H:/Informatik/PythonApplications/iqbal/iqbal/initial.dat"
+# Result File from iqbal
+qtopo = r"H:/Informatik/PythonApplications/iqbal/iqbal/qtopo.txt"
+# CNR4 Table
 fileDF = r"h:/Geography/aerosol/Data/zwischenergebnis/CNR4-2013.dat"
-fileResult = r"H:/Informatik/PythonApplications/iqbal/iqbal/qtopo.txt"
+# Final Result (CNR4 Table with iqbal Value)
 table = "h:/Geography/aerosol/Data/zwischenergebnis/CNR4-2013_iqbal.dat"
 
 transDict = {}
 
+
+#### START ####
 fDF = open(fileDF,"r")
-
+# Iterate over every CNR4 Value
 for index, DFlines in enumerate(fDF):
-    
-    dateList=[]
-    timeList=[]
-
     print(index,"bob")
 
     if index > 0:
-        tmpDF = DFlines.split(";",1)
+		
+		#### SEPERATE TIMESTAMP ####
+        # Split by Seperator
+		tmpDF = DFlines.split(";",1)
+		# tmpDF[0] = Date; tmpDF[1] = All other columns
+		# Seperate Date from Time:
         tmpDF = tmpDF[0].split(" ")
         date = tmpDF[0]
         time = tmpDF[1]
         
+		# Stores Date(0 = Year, 1 = Month, 2 = Day)
         dateList = date.split("-")
+		# Stores Time(0 = Year, 1 = Month, 2 = Day)
         timeList = time.split(":")
-
+		
+		# Store Day, Month, schaltjahr and time
         day = dateList[2]
         month = dateList[1]
         
@@ -36,12 +46,11 @@ for index, DFlines in enumerate(fDF):
         else:
             schaltjahr = "0"
 
-
         hour = timeList[0]
         minute = timeList[1]
         seconds = timeList[2]
 
-        # Nullen eliminieren
+        # Nullen eliminieren. "01" needs to be "1"
         if day[0] == "0":
             day = day[1]
         if month[0] == "0":
@@ -61,63 +70,57 @@ for index, DFlines in enumerate(fDF):
                 seconds = "0"
             else:
                 seconds = seconds[1]
-
-        f = open(file,"r")
-        ###########################################
-        tmpList = []
-
+		
+		#### OPEN IQBAL ####
+        f = open(initalFile,"r")
         for idx, lines in enumerate(f):
             
-            if idx ==  2:
+			# Day
+            if idx ==  2: 
                 tmpSplitList = lines.split('\t')
                 lines = day + "\t" + tmpSplitList[1]
                 tmpList.append(lines)
+			# Month
             elif idx == 3:
                 tmpSplitList = lines.split('\t')
                 lines = month + "\t" + tmpSplitList[1]
                 tmpList.append(lines)
+			# Leap Year
             elif idx == 4:
                 tmpSplitList = lines.split('\t')
                 lines = schaltjahr + "\t" + tmpSplitList[1]
                 tmpList.append(lines)
+			#Time 
             elif idx == 5:
                 tmpSplitList = lines.split('!')
                 lines = hour + "  " + minute + "   " + seconds + "     !" + tmpSplitList[1]
                 tmpList.append(lines)
+			# Kill every Line greater than Line 19 (qtopo)
             elif idx > 19:
                 pass
+			# Append every other Line
             else:
                 #tmpSplitList = lines.split('!')             
                 #lines = tmpSplitList[0] + "!" + tmpSplitList[1]
                 tmpList.append(lines)
-
-
         f.close()
 
-        # Write new lines to file
-        # Open file stream
-        g = open(file, 'w' ,) #encoding ='utf8')  
+		#### WRITE IQBAL ####
+        # Write new lines to initalFile
+        g = open(initalFile, 'w' ,) #encoding ='utf8')  
 
         # Write line by line
         for linesW in tmpList:
-
             g.write(linesW)
-
-        # Close file stream
         g.close()	
 
-        #subprocess.call("h:/Geography/aerosol/dir_aer/dir_aer.exe")
+		#### EXECUTE IQBAL ####
         cmd = 'H:/Informatik/PythonApplications/iqbal/iqbal/iqbal_v2.exe'
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        #for line in p.stdout:
-        #    print (line)
-        #Press('ENTER')
         p.wait()
-        #Press('ENTER')
-        #print (p.returncode)
-        #p.kill
-
-        e = open(fileResult,"r")
+       
+		#### READ RESULTS ####
+        e = open(qtopo,"r")
 
         for rIdx, rLines in enumerate(e):
             if rIdx == 47:
